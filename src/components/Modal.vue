fc9740a25d2d3cb8b1aaae3e3dbf703cfc6818d1
Shoko118 <secast118@gmail.com>
2021-12-23 17:29:44 +0800

    self-learn for modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    v-if="trueVisible"
+    class="bg-gray-300 min-h-screen fixed inset-0 bg-opacity-60"
+  >
+    <div class="bg-white shadow-lg rounded-lg w-5/12 h-2/5 mt-14 mx-auto">
+      <button
+        @click="$emit('close')"
+        class="bg-green-500 py-1 px-3 rounded-xl mt-24"
+      >
+        Close modal
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    trueVisible: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-center pt-2">
+    <button
+      @click="falseVisible = true"
+      class="bg-pink-400 py-2 px-3 text-white rounded-xl text-xl"
+    >
+      Open Modal {{ falseVisible }}
+    </button>
+    <Modal :trueVisible="falseVisible" @close="falseVisible = false" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import Modal from "../components/Modal.vue";
+export default {
+  setup() {
+    const falseVisible = ref(false);
+    return {
+      falseVisible,
+    };
+  },
+  components: { Modal },
+};
+</script>
